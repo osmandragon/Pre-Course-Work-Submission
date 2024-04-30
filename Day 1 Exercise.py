4424613d55eb2c2838bbb673a8731534f046cdb6
osmandragon <167456570+osmandragon@users.noreply.github.com>
2024-04-30 13:49:10 -0500

    Add files via upload

---
+++ Day 1 Exercise.py
@@ -0,0 +1,43 @@
+#EXERCISE 1: Echo String
+
+#first_name = input("what is your name?")
+#print(first_name,"That's an Awesome name!")
+
+
+#EXERCISE 2: Adding a number to an Integer
+
+
+#input("what is your favorite number?")
+#num1 = int(input("Type in a number"))
+#print(num1 +1)
+
+
+#EXERCISE 3: Adding a number to a float
+
+
+#num1 = float(input("Type in a number"))
+#print(num1 +.5)
+
+
+#EXERCISE 4: Adding Two Floats
+
+
+#num1 = float(input("Type in a number"))
+#num2 = float(input("Type in the second number"))
+#print(num1+num2)
+
+
+#EXERCISE 5: Multiplying Floats
+
+
+#num1 = float(input("Type in a number"))
+#num2 = float(input("Type in the second number"))
+#print(num1*num2)
+
+
+#EXERCISE 6: Dividing Integers
+
+
+#num1 = int(input("Type in a number"))
+#num2 = int(input("Type in the second number"))
+#print(num1/num2)
